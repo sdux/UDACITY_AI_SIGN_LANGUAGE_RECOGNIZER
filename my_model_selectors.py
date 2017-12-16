ff0fe660e4da4365fd31010a89b379f18fefe270
S Dux <sduxbury@gmail.com>
2017-12-15 17:12:07 -0700

    completed section 2, unit tests pass

--- my_model_selectors.py
+++ my_model_selectors.py
@@ -66,18 +66,51 @@ class SelectorBIC(ModelSelector):
 
     http://www2.imm.dtu.dk/courses/02433/doc/ch6_slides.pdf
     Bayesian information criteria: BIC = -2 * logL + p * logN
+    #parameters = n_comp ** 2 + 2 * n_comp *n_features-1
+    #score = (-2 *logLikelihood + parameters *math.log(number of datapoints))
     """
-
-    def select(self):
-        """ select the best model for self.this_word based on
-        BIC score for n between self.min_n_components and self.max_n_components
-
-        :return: GaussianHMM object
-        """
+    def train_model(self, n_states):
         warnings.filterwarnings("ignore", category=DeprecationWarning)
-
+        try:
+            hmm_model = GaussianHMM(n_components=n_states, covariance_type="diag", n_iter=1000,
+              random_state=self.random_state, verbose=False).fit(self.X, self.lengths)
+            
+            score = hmm_model.score(self.X, self.lengths)
+            
+            if self.verbose:
+                print("model created for {} with {} states".format(self.this_word, n_states))
+                print("model score: {}".format(score))
+ 
+            return hmm_model, score
+        
+        except:
+            if self.verbose:
+                print("failure on {} with {} states".format(self.this_word, n_states))
+        
+    def select(self):
+        '''
+        returns selected model based on lowest BIC score
+        '''
+        best_BIC_score = np.inf
+        selected_model = None
+        
+        for n_comp in range(self.min_n_components,self.max_n_components+1):
+            # score is the logLikelyhood
+            # model is the HMM model created for n_components
+            try:
+                hmm_model, score = self.train_model(n_comp)
+            except:
+                pass
+            # Calculate BIC score
+            parameters = n_comp **2 + 2 * n_comp * hmm_model.n_features - 1
+            BIC_score = (-2 * score + parameters * math.log(len(self.X)))
+            
+            if BIC_score < best_BIC_score:
+                selected_model = hmm_model
+                best_BIC_score = BIC_score
+                
         # TODO implement model selection based on BIC scores
-        raise NotImplementedError
+        return selected_model
 
 
 class SelectorDIC(ModelSelector):
@@ -90,20 +123,118 @@ class SelectorDIC(ModelSelector):
     DIC = log(P(X(i)) - 1/(M-1)SUM(log(P(X(all but i))
     '''
 
-    def select(self):
+    def train_model(self, n_states):
         warnings.filterwarnings("ignore", category=DeprecationWarning)
-
-        # TODO implement model selection based on DIC scores
-        raise NotImplementedError
-
-
+        try:
+            hmm_model = GaussianHMM(n_components=n_states, covariance_type="diag", n_iter=1000,
+              random_state=self.random_state, verbose=False).fit(self.X, self.lengths)
+            
+            score = hmm_model.score(self.X, self.lengths)
+            
+            if self.verbose:
+                print("model created for {} with {} states".format(self.this_word, n_states))
+                print("model score: {}".format(score))
+ 
+            return hmm_model, score
+        
+        except:
+            if self.verbose:
+                print("failure on {} with {} states".format(self.this_word, n_states))
+        
+    def select(self):
+        '''
+        returns selected model based on highest DIC score
+        '''
+        best_DIC_score = -np.inf
+        selected_model = None
+  
+        for n_comp in range(self.min_n_components,self.max_n_components+1):
+            # score is the logLikelyhood
+            # model is the HMM model created for n_components
+            try:
+                hmm_model, score = self.train_model(n_comp)
+            except:
+                pass
+            # Calculate DIC score
+            # Sum all scores for words other than current word.
+            summed_score = 0
+            n_words = 0
+            for word in self.hwords:
+                # pass through if word is the current word
+                if word == self.this_word:
+                    continue
+                else:
+                    n_words += 1
+                    summed_score += hmm_model.score(self.X, self.lengths)
+          
+            DIC_score = score - (summed_score/n_words)
+           
+            if DIC_score > best_DIC_score:
+                selected_model = hmm_model
+                best_DIC_score = DIC_score
+                
+        # TODO implement model selection based on BIC scores
+        return selected_model
+    
+    
 class SelectorCV(ModelSelector):
     ''' select best model based on average log Likelihood of cross-validation folds
 
     '''
 
-    def select(self):
+    def train_model(self, n_states, X_train, length_train, X_test, length_test):
         warnings.filterwarnings("ignore", category=DeprecationWarning)
+        try:
+            hmm_model = GaussianHMM(n_components=n_states, covariance_type="diag", n_iter=1000,
+              random_state=self.random_state, verbose=False).fit(X_train, length_train)
+            
+            score = hmm_model.score(X_test, length_test)
+            
+            if self.verbose:
+                print("model created for {} with {} states".format(self.this_word, n_states))
+                print("model score: {}".format(score))
+ 
+            return hmm_model, score
+        
+        except:
+            if self.verbose:
+                print("failure on {} with {} states".format(self.this_word, n_states))
+        
+    def select(self):
+        '''
+        returns selected model based on highest Cross Validation score
+        '''
+        best_CV_score = -np.inf
+        selected_model = None
+        best_n_comp = None
+        summed_score = 0
 
-        # TODO implement model selection using CV
-        raise NotImplementedError
+        # number of splits, either 3 or number of samples
+        n_splits = min(len(self.lengths), 3)
+
+        for n_comp in range(self.min_n_components,self.max_n_components+1):
+            
+            for cv_train_idx, cv_test_idx in KFold(n_splits).split(self.sequences):
+                # Split data set into train and test sets
+                X_train, length_train = combine_sequences(cv_train_idx, self.sequences)
+                X_test, length_test = combine_sequences(cv_test_idx, self.sequences)
+           
+                # model is the HMM model created for n_components
+                try:
+                    hmm_model, test_score = self.train_model(n_comp, X_train, length_train, X_test, length_test)
+                except:
+                    pass
+                # sum scores for each state
+                summed_score += test_score
+                   
+                # average score per state
+                CV_score = summed_score / n_comp
+           
+            if CV_score > best_CV_score:
+                best_CV_score = CV_score
+                best_n_comp = n_comp
+
+            # Re-calculate selected model and score with full dataset
+            selected_model, score = self.train_model(best_n_comp, self.X, self.lengths, self.X, self.lengths)   
+        # TODO implement model selection based on BIC scores
+        return selected_model
